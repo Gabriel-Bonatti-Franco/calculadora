2d67219680e6b56c8d87c272535b9d5c205d53c4
Gabriel-Bonatti-Franco <gabrielbonatti12@gmail.com>
2023-05-21 14:47:35 -0300

    Atualizando Projeto

--- src/js/script.js
+++ src/js/script.js
@@ -2,58 +2,84 @@ const botoes = document.querySelectorAll('.botao')
 const botaoPolaridade = document.querySelector('.botaoPolaridade')
 const botaoResultado = document.querySelector('.botaoResultado')
 const botaoFuncoes = document.querySelectorAll('.botaoFuncoes')
+const botaoOperadores  = document.querySelectorAll('.botaoOperadores')
 
 const calculo = document.querySelector('.calculo')
 const resultado = document.querySelector('.resultado')
 
-function calc(textoCalculo) {
-    operacoes = ['/','X','-','+']
-    conta = []
-    
-    for (let index = 0; index < textoCalculo.length; index++) {
-    
-    }
+operacoes = ['/','X','-','+']
+var valorAtual = ''
+var conta = []
 
-    operacoes.forEach(elemento => {
+function ColocarNaConta(elemento) {
+    
+    if ((elemento != '')) {
         
-        switch (elemento) {
-            case 'X':
-                
-                mutiplicar = textoCalculo.split(elemento).
+        conta.push((valorAtual))
 
-                conta.push(mutiplicar)
-                alert(conta)
+    }
+    
+}
 
-                break;
-            case '/':
-            
+function clicado(elemento) {
+    elemento.classList.add('clicado')
+}
 
-                break;
-            case '-':
-            
+function desclicado(elemento) {
+    elemento.classList.remove('clicado')
+    console.log(conta)
+}
 
-                break;
-            case '+':
-            
+function calc(arrayConta) {
+    
+    arrayConta.forEach((valor, index) => {
 
-                break;
-            default:
-                break;
+        if (arrayConta.length == 1){
+            console.log('primeiro if')
+            return arrayConta[0]
+        }else if (valor == 'X') {
+            let mutiplicar = arrayConta[index - 1] * arrayConta[index + 1]
+            arrayConta.splice((index-1), 3, mutiplicar)
+
+            console.log('mutiplicando...')
+        }
+        else if(valor == '/'){
+            let dividir = arrayConta[index - 1] / arrayConta[index + 1]
+            arrayConta.splice((index-1), 3, dividir)
+            console.log('dividindo...')
+        }
+        else if(valor == '-'){
+            let subtrair = parseFloat(arrayConta[index - 1]) - parseFloat(arrayConta[index + 1])
+            arrayConta.splice((index-1), 3, subtrair)
+            console.log('subtraindo...')
+        }
+        else if(valor == '+'){
+            // index = arrayConta.indexOf(valor)
+            let somar = parseFloat(arrayConta[index - 1]) + parseFloat(arrayConta[index + 1])
+            arrayConta.splice((index-1), 3, somar)
+            console.log('somando...')
         }
 
     })
-
+    return arrayConta[0]
 }
 
 botoes.forEach(botao => {
 
     botao.addEventListener('mousedown', () => {
-        botao.classList.add('clicado')
+        
+        clicado(botao)
+
+        valorAtual += botao.innerHTML
+        console.log(valorAtual)
         resultado.innerHTML += botao.innerHTML
+
     })
 
     botao.addEventListener('mouseup', () => {
-        botao.classList.remove('clicado')
+    
+        desclicado(botao)
+
     })
 
 })
@@ -79,6 +105,8 @@ botaoFuncoes.forEach((botao, index) => {
                 botao.classList.add('clicado')
                 resultado.innerHTML = ''
                 calculo.innerHTML = ''
+                valorAtual = ''
+                conta = []
             })
         
             botao.addEventListener('mouseup', () => {
@@ -105,16 +133,60 @@ botaoFuncoes.forEach((botao, index) => {
 
 })
 
+botaoOperadores.forEach(botao => {
+    
+    botao.addEventListener('mousedown', () => {
+
+        clicado(botao)
+
+        if (valorAtual!='' && !(valorAtual in operacoes)){
+            ColocarNaConta(valorAtual)
+        }
+        
+        valorAtual = botao.innerHTML
+        ColocarNaConta(valorAtual)
+
+        valorAtual = ''
+
+        resultado.innerHTML += botao.innerHTML
+    })
+
+    botao.addEventListener('mouseup', () => {
+
+        desclicado(botao)
+
+    })
+
+})
+
 botaoResultado.addEventListener('mousedown', () => {
     
-    botaoResultado.classList.add('clicado')
+    clicado(botaoResultado)
+    
     calculo.innerHTML = resultado.innerHTML
-    //resultado.innerHTML = calc(resultado.innerHTML)
-    calc(resultado.innerHTML)
+    
+    ColocarNaConta(valorAtual)
+
+    let total = calc(conta)
+
+    if (total != undefined && total != '') {
+        
+        total = total.length >= 8 ? total : parseFloat(total.toFixed(8))
+
+        //total = total >= 99999999999 ? 
+
+    }else{
+        total = ''
+    }
+    
+    resultado.innerHTML = total
+    valorAtual = total
+
+    conta = []
 })
 
 botaoResultado.addEventListener('mouseup', () => {
-    
-    botaoResultado.classList.remove('clicado')
+
+    desclicado(botaoResultado)
 
 })
